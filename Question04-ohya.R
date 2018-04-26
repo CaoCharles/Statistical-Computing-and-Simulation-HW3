17c9a1c33180bde509d8dfde120eef67d9a7db60
CaoCharles <eric830123@gmail.com>
2018-04-27 02:13:12 +0800

    0426

--- Question04-ohya.R
+++ Question04-ohya.R
@@ -54,7 +54,7 @@ naiveest=function(x,h){
     if (abs(y)<1) {return(1/2)}
     else {return(0)}}
   n=length(x)
-  sx=seq(min(x),max(x),length=500)
+  sx=seq(min(x),max(x),length=100)
   t1=NULL
   for (i in sx){
     t0=NULL
@@ -70,7 +70,7 @@ naiveest=function(x,h){
 kernel_norm=function(x,h){
   w=function(y){ dnorm(y) }
   n=length(x)
-  sx=seq(min(x),max(x),length=500)
+  sx=seq(min(x),max(x),length=100)
   t1=NULL
   for (i in sx){
     t0=NULL
@@ -89,14 +89,67 @@ y3=kernel_norm(x,0.1)            #h=0.1，也可使用其他h值
 par(mfrow=c(1,1))
 plot(sort(x),y1,typ="l",xlab="x",ylab="f(x)",xlim=c(0,1), ylim=c(0,3))
 
-j <- seq(0,1,length=5000)
+j <- seq(0,1,length=500)
+yy <- dbeta(j,2,3)
+plot(yy)
 lines(seq(0,1,length=5000),dbeta(j,2,3),col = "skyblue")
 matplot(cbind(xa,xa),cbind(y2,y3),typ=c("l","l"),
         pch=1,col=c(2,4),lty=c(2:3),main=" Three Density Estimates ",add=TRUE)
 legend(1,0.3,c("histogram(h=0.1)","naive(h=0.1)","kernel(h=0.1)")
        ,col=c(1,2,4),lty=c(1:3),cex=0.8)
+xx <- sort(x)
+yy <- dbeta(xx,2,3)
+y2 <- naiveest(xx,0.1)
+y3 <- kernel_norm(xx,0.1)
+data <- cbind(xx,yy,y2,y3) %>% as.data.frame()
 
+colnames(data) <- c("sample","hist","naive","kernal")
+library(tidyverse)
+ggplot(data = data,aes(x=sample))+
+  geom_histogram(mapping = aes(y=..density..),color="white",fill="gray")+
+  geom_line(mapping = aes(y=hist),color="green",size=1)+
+  geom_line(mapping = aes(y=naive),color="gray",size=1.5,linetype=4)+
+  geom_line(mapping = aes(y=kernal),color="blue",size=1.5,linetype=2)
+ 
 
+#question05
 
+x <- runif(1000,0,1*pi)
+e <- rnorm(1000,0,0.9)
+data <- sin(x)+e
+data
+naiveest(data,0.1)
+# naive density estimator
+naiveest=function(x,h){
+  w=function(y){
+    if (abs(y)<1) {return(1/2)}
+    else {return(0)}}
+  n=length(x)
+  sx=seq(min(x),max(x),length=1000)
+  t1=NULL
+  for (i in sx){
+    t0=NULL
+    for (j in x){
+      wei=w((i-j)/h)
+      t0=c(t0,wei)}
+    y=1/n*sum(1/h*t0)
+    t1=c(t1,y)}
+  return(t1)}
 
+# kernel density estimator
+# norm
+kernel_norm=function(x,h){
+  w=function(y){ dnorm(y) }
+  n=length(x)
+  sx=seq(min(x),max(x),length=1000)
+  t1=NULL
+  for (i in sx){
+    t0=NULL
+    for (j in x){
+      wei=w((i-j)/h)
+      t0=c(t0,wei)}
+    y=1/n*sum(1/h*t0)
+    t1=c(t1,y)}
+  return(t1)}
 
+ya <- naiveest(sort(data),0.1)
